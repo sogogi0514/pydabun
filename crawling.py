fb0defbc571d7767ae04a81e3668b631bcd13913
Esperise <esperis@naver.com>
2025-04-10 19:21:46 +0900

    gptexample.py에서 1.comment 가져오기 실패 해결 2. url에서 나온 oid_2로 언론사 이름을 반환하는 함수 get_press_name 생성 3.get_press_name에서 반환된 언론사 이름으로 폴더 생성 + 언론사이름+숫자 가 이름인 유저이름과 댓글내용을 column으로 가지는 엑셀 저장 4. 나이정보 가져오는 코드 일부 작성 ------------------- 1.이름을 언론사_뉴스제목으로 하는 코드로 변경

--- crawling.py
+++ crawling.py
@@ -17,6 +17,7 @@ headers = {
 def news_url(query):
     page = 1
     url_list = []
+    title_list = []
 
     while page <= 151:
         url = (
@@ -25,15 +26,22 @@ def news_url(query):
                 "&sort=0&photo=0&field=0&pd=1&ds=&de=&cluster_rank=129&mynews=0&office_type=0"
                 "&office_section_code=0&news_office_checked=&nso=so:r,p:1w,a:all&start=" + str(page)
         )
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+            'referer':url
+        }
 
         response = requests.get(url, headers=headers)
         html = response.text
         soup = BeautifulSoup(html, "lxml")
         atags = soup.select('.news_tit')
 
+
         for i in atags:
             if "https://n.news.naver.com/" in i['href']:
                 url_list.append(i['href'])
+                news_title_name=get_title_name(i)
+                title_list.append(news_title_name)
 
         if len(atags) < 15:
             break
@@ -41,7 +49,10 @@ def news_url(query):
         page += 15
         time.sleep(0.5)  # 너무 빠르게 요청하지 않도록 sleep
 
-    return url_list
+    return url_list ,title_list # -> zip으로 묶거나 받는 변수를 2개 나 튜플로 해야함
+
+
+
 
 # 뉴스 연령대 데이터가 필요하면 여기서 더 추가해서 사용 가능함
 # def getAge(url_list,headers,url_list_num,oid_1,oid_2):
@@ -68,6 +79,10 @@ def news_url(query):
 #     return gender_male, gender_female, ages_group_10, ages_group_20, ages_group_30, ages_group_40, ages_group_50, ages_group_60
 
 #oid_2를 매개변수로 넣으면 언론사 이름이 string으로 return되는 함수입니다.
+
+
+#언론사 id(oid_2)를 언론사 이름으로 바꾸는 함수  ex) "023" -> "연합뉴스"
+# html요청 없이 따로 딕셔너리 형태로 만들어서 사용하면  더 빨라지는데 언론사 수가 생각보다 많아서 그냥 이렇게 했습니다.
 def get_press_name(press_id):
     url_company='https://news.naver.com/main/officeList.naver'
     html_company = urllib.request.urlopen(url_company).read()
@@ -80,22 +95,27 @@ def get_press_name(press_id):
             return news_name
 
 
-
-
+# 뉴스 이름을 html에서 가져오는 함수- news_url 함수 안에서 사용해서 별도의 html요청 없이 링크를 가져오려고 하는 html에서 추출
+def get_title_name(atag):
+    news_title_name=atag.find('div').text.strip()
+    print(news_title_name)
+    return news_title_name
 
 
 
 # 댓글 수집 함수
-def comment(url_list):
+def comment(url_list,news_title_list):
     total_comment = []
     url_list_num=0
-
+    news_title_list_num=0
     for url_ex in url_list:
         url = url_ex.split('?')[0]
         oid_1 = url.split('/')[-1]
         oid_2 = url.split('/')[-2]
         i = 1
         url_list_num+=1
+
+
         # getAge(url_list,headers,url_list_num,oid_1,oid_2)
 
 
@@ -130,6 +150,7 @@ def comment(url_list):
             response.encoding = "UTF-8-sig"
             res = response.text.replace("_callback(", "")[:-2]
 
+            news_title_list_num += 1
             try:
                 temp = json.loads(res)
                 comment_list = temp['result'].get('commentList', [])
@@ -142,16 +163,14 @@ def comment(url_list):
                 comment_list_sum=list(zip(user_nickname, comments))
                 col=['작성자','내용']
                 df=pd.DataFrame(comment_list_sum, columns=col)
-                news_name=get_press_name(oid_2)
+                news_name=get_press_name(oid_2) #언론사 id를 언론사 이름 str로 바꾸는 함수
                 folder_path=f'./{news_name}'
-                os.makedirs(folder_path, exist_ok=True)
-                file_name= f"{news_name}1.xlsx"
-                same_file_counter=1;
+                os.makedirs(folder_path, exist_ok=True) #이 py가 있는 경로에 "언론사이름"을 이름으로 하는 폴더 생성, 존재시 넘어감
+                file_name= f"{news_name}_{news_title_list[news_title_list_num]}.xlsx" #파일 이름을 언론사_뉴스제목 으로 정의
                 while os.path.exists(f"{folder_path}/{file_name}"):
                     print(f"{folder_path}/{file_name} 이미 존재함. ")
-                    same_file_counter+=1
-                    file_name= f"{news_name}{same_file_counter}.xlsx"
-                df.to_excel(f"{folder_path}/{file_name}", index=False)
+                    break;
+                df.to_excel(f"{folder_path}/{file_name}", index=False) #엑셀파일로 저장 다른 형식으로 바꾸고자 하면  .xlsx와 이 메소드 바꾸면 됨.
 
 
                 if len(comments) < 97:
@@ -167,9 +186,11 @@ def comment(url_list):
 
 
 query = "탄핵"  # 원하는 검색어
-news_links = news_url(query)
+encoded_query = urllib.parse.quote(query)
+
+news_links, news_title = news_url(encoded_query)
 print(f"{len(news_links)}개의 뉴스 링크 수집 완료")
 print(news_links)
 
-all_comments = comment(news_links)
+all_comments = comment(news_links,news_title)
 print(f"{len(all_comments)}개의 댓글 수집 완료")
